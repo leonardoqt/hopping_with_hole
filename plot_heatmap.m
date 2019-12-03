c552a755c4a09daca35cbe1c28791ce67d329184
Tian Qiu <tianq@sas.upenn.edu>
2019-12-03 14:59:51 -0500

    uncomment heatmap.m

--- plot_heatmap.m
+++ plot_heatmap.m
@@ -1,19 +1,19 @@
-%close all;
-%clear all;
-%num = 2000;
-%data = dlmread('heatmap.dat');
-%sz = size(data,1)/num;
-%data = permute(reshape(permute(data,[2,1]),[3,sz,num]),[2,1,3]);
-%colors = [0.1,0.1,0.8;1,0,0;0,0,0;0.9,0.9,0.1];
-%colormap(colors);
-%set(gcf, 'Position',  [100, 100, 1000, 1000]);
-%for t1=1:num
-%	pp = scatter(data(:,1,t1),data(:,2,t1),300,data(:,3,t1),'filled');
-%	F(t1) = getframe(gcf);
-%	t1
-%	pause(0.03)
-%end
-
+close all;
+clear all;
+num = 2000;
+data = dlmread('heatmap.dat');
+sz = size(data,1)/num;
+data = permute(reshape(permute(data,[2,1]),[3,sz,num]),[2,1,3]);
+colors = [0.1,0.1,0.8;1,0,0;0,0,0;0.9,0.9,0.1];
+colormap(colors);
+set(gcf, 'Position',  [100, 100, 1000, 1000]);
+for t1=1:num
+	pp = scatter(data(:,1,t1),data(:,2,t1),300,data(:,3,t1),'filled');
+	F(t1) = getframe(gcf);
+	t1
+	pause(0.03)
+end
+% make avi
 writerObj = VideoWriter('myVideo.avi');
 writerObj.FrameRate = 20;
 open(writerObj);
